62b78f5509367a20eaba47e19425d90067042568
SahreyarArafat <132135025+SofolotarDak@users.noreply.github.com>
2023-07-05 02:32:10 +0600

    Update multer-upload.util.js

--- utils/multer-upload.util.js
+++ utils/multer-upload.util.js
@@ -2,48 +2,44 @@
 const multer = require('multer');
 const path = require('path');
 
-// renaming the uploaded file and defind it's storage
+// Renaming the uploaded file and defining its storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/Uploads');
     },
     filename: (req, file, cb) => {
-        const fileExt = path.extname(file.originalname);
-        const fileName = `${file.originalname
-            .replace(fileExt, '')
-            .toLowerCase()
-            .split(' ')
-            .join('-')}-${Date.now()}`;
-        cb(null, fileName + fileExt);
+        try {
+            const fileExt = path.extname(file.originalname);
+            const fileName = `${file.originalname
+                .replace(fileExt, '')
+                .toLowerCase()
+                .split(' ')
+                .join('-')}-${Date.now()}`;
+            cb(null, fileName + fileExt);
+        } catch (error) {
+            cb(error);
+        }
     },
 });
 
-// prepare the multer upload object
+// Prepare the multer upload object
 const upload = multer({
     storage,
     limits: {
         fileSize: 1000000, // 1MB
     },
     fileFilter: (req, file, cb) => {
-        if (file.fieldname === 'storyimage') {
-            try {
+        try {
+            if (file.fieldname === 'storyimage') {
                 const fileExt = path.extname(file.originalname);
-                const allowedExtensions = ['.png', '.jpg'];
-                const maxSize = 1 * 1024 * 1024; // 1MB
-
-                if (
-                    allowedExtensions.includes(fileExt) &&
-                    file.size <= maxSize
-                ) {
+                if (fileExt === '.png' || fileExt === '.jpg') {
                     cb(null, true);
                 } else {
-                    throw new Error(
-                        'ক্রুটিপূর্ণ ছবি। শুধুমাত্র PNG, JPG, JPEG, এবং GIF এক্সটেনশন যুক্ত ছবি আপলোড করতে পারবেন এবং এর সাইজ সর্বোচ্চ 1MB পর্যন্ত হতে হবে।'
-                    );
+                    cb(null, false);
                 }
-            } catch (error) {
-                cb(error);
             }
+        } catch (error) {
+            cb(error);
         }
     },
 });
